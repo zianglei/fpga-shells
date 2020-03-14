edf6870dd0c7ad708d91645d1eb54e9908681100
zianglei <zianglei@126.com>
2020-03-14 13:46:29 +0800

    Add read checkpoint

--- xilinx/common/tcl/opt.tcl
+++ xilinx/common/tcl/opt.tcl
@@ -5,3 +5,5 @@ opt_design -directive Explore
 
 # Checkpoint the current design
 write_checkpoint -force [file join $wrkdir post_opt]
+
+read_checkpoint -incremental [file join $wrkdir post_opt.dcp]
